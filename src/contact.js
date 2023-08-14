9dcca7226f1f73b16053866e31f4b4e505aeb58a
AnthonyEspinozaCS <aespinoza.cs@gmail.com>
2023-08-14 14:24:19 -0700

    Nav and footer link changes.

--- src/contact.js
+++ src/contact.js
@@ -8,15 +8,15 @@ const public_key = "ECZnDfndQHxfORbGd";
 window.onload = function () {
   document.getElementById("contact-form").addEventListener("submit", function (event) {
     event.preventDefault();
-    // generate a five digit number for the contact_number variable
-    this.contact_number.value = (Math.random() * 100000) | 0;
+    this.client.value = "info@georgeyancci.com";
+    this.client_name.value = "George";
     // these IDs from the previous steps
     emailjs.sendForm("contact_service", "contact_form", this).then(
-      function () {
-        console.log("SUCCESS!");
+      () => {
+        alert("Your form has been submitted successfully!!");
       },
-      function (error) {
-        console.log("FAILED...", error);
+      (error) => {
+        alert(JSON.stringify(error));
       }
     );
   });
